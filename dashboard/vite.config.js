c499dd1c88c5890b43d0dbcb5d3847dd35d367fb
Ihthisham Raafee <ihthitysschool2016@gmail.com>
2025-10-31 22:55:22 +0530

    Added backend, dashboard, and frontend

---
+++ dashboard/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000 // You can set a different port to avoid conflicts with your frontend app
+  }
+})
