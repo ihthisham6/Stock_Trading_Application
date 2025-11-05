5d40b867d13d45f6709f22bda8bd3ca228898b9b
Ihthisham Raafee <ihthitysschool2016@gmail.com>
2025-11-05 21:41:47 +0530

    Update Navbar

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -113,7 +113,7 @@ function Navbar() {
  <li className="nav-item">
       <a 
         className="nav-link active text-secondary" // Use a secondary, muted color
-        href={`${import.meta.env.VITE_DASHBOARD_URL}/demo`}
+        href={`${import.meta.env.VITE_DASHBOARD_URL}`}
       >
         Demo Login
       </a>
